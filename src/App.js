c9518e0cf9ab862e1902a5f6ec5995cca41d39fb
Pranjali bramhankar <pranjalibramhankar14@gmail.com>
2024-05-22 18:07:56 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,51 @@
+import React, { useState, useEffect } from 'react'
+import Heading from './component/Heading'
+import Form from './component/Form'
+import './App.css'
+import TodoList from './component/TodoList'
+
+const App = () => {
+  const initialState = JSON.parse(localStorage.getItem('todos')) || []
+  const [input, setInput] = useState('')
+  const [todos, setTodos] = useState(initialState)
+  const [editTodo, setEditTodo] = useState(null)
+
+  useEffect(() => {
+    localStorage.setItem('todos', JSON.stringify(todos))
+  }, [todos])
+
+  return (
+    <div className="container">
+      <div className="app-wrapper">
+        <div>
+          <Heading />
+        </div>
+        <div>
+          <task-input />
+          <div>
+            <Form
+              input={input}
+              setInput={setInput}
+              todos={todos}
+              setTodos={setTodos}
+              editTodo={editTodo}
+              setEditTodo={setEditTodo}
+            />
+            {/* {todos.map((todo) => (
+              <h1> {todo.title} </h1>
+            ))} */}
+          </div>
+        </div>
+        <div>
+          <TodoList
+            todos={todos}
+            setTodos={setTodos}
+            setEditTodo={setEditTodo}
+          />
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default App
---
+++ src/component/Heading.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Heading = () => {
+  return (
+    <div className='Heading'>
+     <h1>Todo-list</h1>
+      
+    </div>
+  )
+}
+
+export default Heading
